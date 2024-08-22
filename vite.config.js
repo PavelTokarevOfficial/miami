de15c7d5fb4f3d015e8599aea615b514d4c7fb7b
PavelTokarevOfficial <64586673+PavelTokarevOfficial@users.noreply.github.com>
2024-08-22 10:47:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,14 @@ export default defineConfig({
       '@components': '/src/components',
     },
   },
+  server: {
+    // Важно для работы в Docker
+    host: '0.0.0.0',  // Позволяет принимать запросы из внешней сети
+    port: 4173,       // Порт, на котором будет работать приложение
+  },
+  build: {
+    outDir: 'dist',   // Папка, в которую будет производиться сборка
+  },
   base: "/miami",
   plugins: [vue()],
 })
